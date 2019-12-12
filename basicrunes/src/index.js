0b489ba61939d8108494ecb6e71ec4cbdd776fe4
Christian T Flink <chri62aa@edu.easj.dk>
2019-12-12 17:11:38 +0100

    further cleanup and added Favicon

--- basicrunes/src/index.js
+++ basicrunes/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+//ReactDOM tager imod 2 parametre 1: hvad ønsker jeg at render , 2 hvor skal det renders
 ReactDOM.render(<App />, document.getElementById('root'));
 
