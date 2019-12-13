f5ec7a4cf55f6025a4acbcb9b1a2e26cd57f09b5
Christian T Flink <chri62aa@edu.easj.dk>
2019-12-13 01:15:33 +0100

    components folder

--- basicrunes/src/index.js
+++ basicrunes/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import RuneWiz from './RuneWiz';
+import RuneWiz from './components/RuneWiz';
 
 // ReactDOM tager imod 2 parametre: 
 // 1 hvad skal renderes, 
