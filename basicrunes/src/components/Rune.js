96434fe1d46d28a22a7b92e9ea445cfafcd1ff6f
Christian T Flink <chri62aa@edu.easj.dk>
2019-12-15 00:06:51 +0100

    css hover selector

--- basicrunes/src/components/Rune.js
+++ basicrunes/src/components/Rune.js
@@ -2,22 +2,22 @@ import React from 'react'
 // import data from '../data'
 // import RuneWiz from './RuneWiz'
 
-function myfunction(){
-    document.getElementById('hej').style.border= 'solid'
-    document.getElementById('hej').style.borderColor= 'darkblue'
-}
+// function myfunction(){
+//     document.getElementById('hej').style.border= 'solid'
+//     document.getElementById('hej').style.borderColor= 'darkblue'
+// }
+//  onMouseOver={myfunction} onMouseLeave={showEffects} 
 
-function showEffects(){
-    document.getElementById('hej').style.border= 'solid'
-    document.getElementById('hej').style.borderColor= 'grayscale'
-}
+function showAlert(){
+    alert('herj')
+    }
 
 class Rune extends React.Component{
 
     render (){
         return(
         <div className="Rune" >
-        <img id={'hej'} src={this.props.runeobject.imgUrl} alt={this.props.runeobject.name} onMouseOver={myfunction} onMouseLeave={showEffects} title={this.props.runeobject.name}/>
+        <img id={'Rune'} src={this.props.runeobject.imgUrl} alt={this.props.runeobject.name} title={this.props.runeobject.name} onClick={showAlert}/>
 
             <p>{this.props.runeobject.name}</p>
             <p>{this.props.runeobject.lvlReq}</p>
@@ -29,6 +29,5 @@ class Rune extends React.Component{
 
     }
 }   
-     
 
 export default Rune
